0c2bbba1f30e7797d4ed04e9ebde6e2e9b55b0d5
Risha Ranjan <risharanjan@Rishas-MacBook-Air.local>
2022-12-01 11:15:38 -0500

    figured out time type err?

--- flask-app/src/managers/managers_api.py
+++ flask-app/src/managers/managers_api.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify, make_response
 import json
 from src import db
+from datetime import timedelta
 
 managers = Blueprint('managers', __name__)
 
@@ -26,10 +27,20 @@ def get_all_times():
     cursor.execute('select * from Orders')
     row_headers = [x[0] for x in cursor.description]
     json_data = []
+    type_data = []
     theData = cursor.fetchall()
     for row in theData:
-        json_data.append(dict(zip(row_headers, row)))
+        rowList = list(row)
+        for index, item in list(enumerate(rowList)):
+            if isinstance(item, timedelta):
+                rowList[index] = str(rowList[index])
+        rowTuple = tuple(rowList)
+        json_data.append(dict(zip(row_headers, rowTuple)))
     the_response = make_response(jsonify(json_data))
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
     return the_response
+    # the_response = make_response(jsonify(json_data))
+    #the_response.status_code = 200
+    #the_response.mimetype = 'application/json'
+    #return the_response
