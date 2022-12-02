b0b9dc7445d6000e1b563f80fb57422824c2a025
kstuntz <stuntz.k@northeastern.edu>
2022-12-02 14:36:07 -0500

    bit of a post route for orders

--- flask-app/src/customers/customers.py
+++ flask-app/src/customers/customers.py
@@ -35,5 +35,31 @@ def get_customer(custID):
     the_response.mimetype = 'application/json'
     return the_response
 
+@customers.route("/product/ingr/form")
+def get_prodIngr_form():
+    return """
+    <h2>New Ingredient</h2>
+    <form action="/cust/product/ingr/form" method="POST">
+    <label for="ingrId">IngredientId:</label><br>
+    <input type="text" id="ingrId" name="ingrId" value=""><br>
+    <label for="quantity">Quantity:</label><br>
+    <input type="text" id="quantity" name="quantity" value=""><br>
+    <input type="submit" value="Submit">
+    </form> 
+    """
+
+@customers.route("/product/ingr/form", methods = ['POST'])
+def post_prodIngr_form():
+    ingr_id = request.form['ingrId']
+    quant = request.form['quantity']
+    cursor = db.get_db().cursor()
+    cursor.execute('select max(ProductId) from Product')
+    all = cursor.fetchall()
+    for row in all:
+        data = row[0]
+    data = data + 1
+    return f'<h1>IngrId: {ingr_id} Quantity: {quant} ProductId: {data}</h1>'
+
+
 
 
