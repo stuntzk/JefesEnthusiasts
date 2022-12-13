e9bead95202df5e27c84132c7304c425031f451f
kstuntz <stuntz.k@northeastern.edu>
2022-12-13 09:09:48 -0500

    clean up customer code

--- flask-app/src/customers/customers.py
+++ flask-app/src/customers/customers.py
@@ -7,7 +7,7 @@ from src import db
 
 customers = Blueprint('customers', __name__)
 
-# Get all customers from the DB
+# get all customers from the database
 @customers.route('/all', methods=['GET'])
 def get_customers():
     cursor = db.get_db().cursor()
@@ -22,7 +22,7 @@ def get_customers():
     the_response.mimetype = 'application/json'
     return the_response
 
-# Get customer detail for customer with particular userID
+# Get customer details for customer with particular userID
 @customers.route('/<custID>')
 def get_customer(custID):
     cursor = db.get_db().cursor()
@@ -37,13 +37,16 @@ def get_customer(custID):
     the_response.mimetype = 'application/json'
     return the_response
 
-# adds a product into the database after the user has it. All of the product ingredients are added individuals
+# allows for the user to add a product into the database through the order form in the appsmith application. during
+# this process, all the product ingredients are also ordered
 @customers.route("/product", methods = ['POST'])
 def add_product():
     cursor = db.get_db().cursor()
     type = request.form['typeid']
+    # increase the product id by 1
     cursor.execute('select max(ProductId) from Product')
     prod = cursor.fetchone()[0] + 1
+    # determines the price of the product
     query = f'SELECT BasePrice FROM FoodType where TypeId = {type}'
     cursor.execute(query)
     price = cursor.fetchone()[0]
@@ -57,16 +60,19 @@ def add_product():
         price = price + cursor.fetchone()[0]
     cursor.execute('select max(OrderId) from Orders')
     order = request.form['orderNum']
+    # adds the product into the database
     query = f'INSERT INTO Product(ProductId, Price, OrderId, TypeId) VALUES ({prod}, {price}, {order}, {type})'
     cursor.execute(query)
     db.get_db().commit()
+    # adds all the product ingredients into the database
     for x in arr:
         query = f'INSERT INTO ProductIngredient(ProductId, IngredientId, Quantity) VALUES ({prod}, {x}, 1)'
         cursor.execute(query)
         db.get_db().commit()
     return f'{price}'
 
-# completes the proper steps after the user places submit on the order (updates the order to have the proper information)
+# completes the proper steps after the user places submit on the order
+# (updates the order to have the proper information)
 @customers.route('/submit', methods = ['POST'])
 def update_order():
     cursor = db.get_db().cursor()
@@ -83,12 +89,9 @@ def update_order():
 # updates the information for each order after the user presses submit
 def db_update_order(store, price, orderId):
     cursor = db.get_db().cursor()
-    minutes = random.randint(2,40)
-    seconds = random.randint(0,59)
-    cookTime = f'00:{minutes}:{seconds}'
     cursor.execute(f'UPDATE Orders SET StoreId = {store}, TotalPrice = {price}, OrderDate = now(), TimeOrdered = now(), TimeCompleted = now(), TimeToMake = time(0) WHERE OrderId = {orderId}')
     db.get_db().commit()
-    return "hi"
+    return "updated"
 
 # determines all the orders a single customer has placed
 @customers.route('/orders/<custId>', methods=['GET'])
@@ -167,32 +170,18 @@ def new_order():
     query = f'INSERT INTO Orders(OrderId, CustomerId) VALUES ({order}, {cust})'
     cursor.execute(query)
     db.get_db().commit()
-    return "hi"
+    return "partial order in system"
 
-# Get all the products from the database
+# Get all the food types from the database
 @customers.route('/types', methods=['GET'])
 def get_foodTypes():
-    # get a cursor object from the database
     cursor = db.get_db().cursor()
-
-    # use cursor to query the database for a list of products
     cursor.execute('select * from FoodType')
-
-    # grab the column headers from the returned data
     column_headers = [x[0] for x in cursor.description]
-
-    # create an empty dictionary object to use in
-    # putting column headers together with data
     json_data = []
-
-    # fetch all the data from the cursor
     theData = cursor.fetchall()
-
-    # for each of the rows, zip the data elements together with
-    # the column headers.
     for row in theData:
         json_data.append(dict(zip(column_headers, row)))
-
     return jsonify(json_data)
 
 # returns all the stores with their id and name
